9beb7fdf3038a31cbbff3db31fe020e567a9c346
DESKTOP-VM7NHVH\kukudocs <jaeyeonjung3@gmail.com>
2024-01-22 21:13:55 +0900

    initial commit

---
+++ prepare/front/components/AppLayout.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const AppLayout = ({ children }) => { // children = react nodes. return 안에 들어갈 수 있는 모든 것
+    return (
+        <div>
+            <div>공통메뉴</div>
+            {children}
+        </div>
+    );
+};
+
+AppLayout.propTypes = {
+    children: PropTypes
+};
+
+export default AppLayout;
